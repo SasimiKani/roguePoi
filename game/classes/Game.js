1b936d1d4f6d1f4ef79f1a7aff2842acbdbd063a
kaniko <kyu9iro@gmail.com>
2025-05-12 23:25:19 +0900

    スキル復元修正

--- game/classes/Game.js
+++ game/classes/Game.js
@@ -1094,7 +1094,7 @@ class Game {
 		this.enemies = this.enemies.map(entity => {
 			const e = new (eval(entity.constructor.name))()
 			Object.entries(entity).forEach(([k, v]) => {
-				if (typeof v === "function") return
+				if (["function", "object"].includes(typeof v)) return
 				e[k] = v
 			})
 			return e
